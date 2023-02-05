1bb2954cfb03958559d9f630329622596d2d2930
archer2o2x <31862296+archer2o2x@users.noreply.github.com>
2023-02-05 14:30:27 +0000

    Added two starter levels

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoScene(Scenes NewScene)
+    {
+        SceneManager.LoadScene(NewScene.ToString());
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public enum Scenes
+    {
+        MainMenu,
+        Credits,
+        Controls,
+        Level01,
+        Level02,
+        Level03,
+        Level04
+    }
+}
